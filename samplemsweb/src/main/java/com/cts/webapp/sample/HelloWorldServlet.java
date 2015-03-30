56ebca68731d5cf79354e884a1947c4a1bd2b74f
gaje85 <gaje85@yahoo.com>
2015-03-30 17:06:24 +0530

    sample code
    sample for docker

---
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -0,0 +1,24 @@
+package com.cts.webapp.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1031422249396784970L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		out.print("<h1>Hello World from "+new java.util.Date()+"</h1>");
+		out.flush();
+		out.close();	}
+}
