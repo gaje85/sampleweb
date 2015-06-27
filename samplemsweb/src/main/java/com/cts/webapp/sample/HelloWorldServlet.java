5f14d330b5279f416c01ac948eec386ffac46674
Gajendran Ganesapandian <gaje85@yahoo.com>
2015-06-27 12:32:28 +0530

    Update HelloWorldServlet.java
    changes

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -18,7 +18,7 @@ public class HelloWorldServlet extends HttpServlet {
 		resp.setContentType("text/html");
 		
 		PrintWriter out = resp.getWriter();
-		out.print("<h1> Updating docker .."+new java.util.Date()+"</h1>");
+		out.print("<h1> Updating code using docker .."+new java.util.Date()+"</h1>");
 		out.flush();
 		out.close();	}
 }
